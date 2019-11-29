bdb9ecf0557b23583b3bd0915bdd0c446d32fe63
Ethan Paek <ethanpaek17@gmail.com>
2019-11-29 15:04:48 -0800

    Randomness included to eliminate bias

--- NetworkXTest/DoWhyModeling.py
+++ NetworkXTest/DoWhyModeling.py
@@ -1,21 +1,22 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from random import random
 # Directed Graphs is more of what want, especially for causal modeling
 DG = nx.DiGraph()
 
-DG.add_weighted_edges_from([("Hacked", "Bug", 0.5),
-                            ("Malware", "Bug", 0.75),
-                            ("Hacked", "Outcome", 0.8),
-                            ("Malware", "Outcome", 0.7),
-                            ("Bug", "Outcome", 0.75),
-                            ("Physical", "Outcome", 0.3),
-                            ("Misuse", "Outcome", 0.5),
-                            ("Error", "Outcome", 0.4),
-                            ("Environment", "Outcome", 0.6),
-                            ("Social", "Outcome", 0.2)
+DG.add_weighted_edges_from([("Hacked", "Bug", random()),
+                            ("Malware", "Bug", random()),
+                            ("Hacked", "Outcome", random()),
+                            ("Malware", "Outcome", random()),
+                            ("Bug", "Outcome", random()),
+                            ("Physical", "Outcome", random()),
+                            ("Misuse", "Outcome", random()),
+                            ("Error", "Outcome", random()),
+                            ("Environment", "Outcome", random()),
+                            ("Social", "Outcome", random())
                             ])
 print(DG.out_degree("Hacked", weight='weight'))
-print(DG.degree("Bug", weight='weight')) # total weight of edges for node 1
+print(DG.degree("Bug", weight='weight')) # total weight of edges for node "Bug"
 print(list(DG.successors("Hacked")))
 print(list(DG.neighbors("Bug")))
 
--- NetworkXTest/test.gml
+++ NetworkXTest/test.gml
@@ -39,51 +39,51 @@ graph [
   edge [
     source 0
     target 1
-    weight 0.5
+    weight 0.5788342684423631
   ]
   edge [
     source 0
     target 3
-    weight 0.8
+    weight 0.8215134433123407
   ]
   edge [
     source 1
     target 3
-    weight 0.75
+    weight 0.8025254545403563
   ]
   edge [
     source 2
     target 1
-    weight 0.75
+    weight 0.28341088620679966
   ]
   edge [
     source 2
     target 3
-    weight 0.7
+    weight 0.8986652077913028
   ]
   edge [
     source 4
     target 3
-    weight 0.3
+    weight 0.8149338730455573
   ]
   edge [
     source 5
     target 3
-    weight 0.5
+    weight 0.5514271888318363
   ]
   edge [
     source 6
     target 3
-    weight 0.4
+    weight 0.7014599227072876
   ]
   edge [
     source 7
     target 3
-    weight 0.6
+    weight 0.5077632129302814
   ]
   edge [
     source 8
     target 3
-    weight 0.2
+    weight 0.8715243317280206
   ]
 ]
