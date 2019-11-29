3cd5cfb5402e48b21f2a33d70d3a6dc16c67a58a
Ethan Paek <ethanpaek17@gmail.com>
2019-11-29 14:34:46 -0800

    RNG to the max

---
+++ NetworkXTest/DoWhyModeling.py
@@ -0,0 +1,22 @@
+import networkx as nx
+# Directed Graphs is more of what want, especially for causal modeling
+DG = nx.DiGraph()
+
+DG.add_weighted_edges_from([("Hacked", "Bug", 0.5),
+                            ("Malware", "Bug", 0.75),
+                            ("Hacked", "Outcome", 0.8),
+                            ("Malware", "Outcome", 0.7),
+                            ("Bug", "Outcome", 0.75),
+                            ("Physical", "Outcome", 0.3),
+                            ("Misuse", "Outcome", 0.5),
+                            ("Error", "Outcome", 0.4),
+                            ("Environment", "Outcome", 0.6),
+                            ("Social", "Outcome", 0.2)
+                            ])
+print(DG.out_degree("Hacked", weight='weight'))
+print(DG.degree("Bug", weight='weight')) # total weight of edges for node 1
+print(list(DG.successors("Hacked")))
+print(list(DG.neighbors("Bug")))
+
+# Writes the gml file for us easily
+nx.write_gml(DG, "test.gml")
